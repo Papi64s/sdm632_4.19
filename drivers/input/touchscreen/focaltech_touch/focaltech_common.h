df1759ab5d85b5e64d723dc6f7bbb6a1ee9a3727
Dhina17 <dhinalogu@gmail.com>
2020-08-09 00:16:07 +0530

    drivers: input: Import FTS touchscreen driver and its minimal changes from Xiaomi
    Change-Id: I6617734ef14cafc561a8fd68e97d145bae4e3cd3
    Signed-off-by: Papi64s <mhmmdsptra@outlook.com>

--- drivers/input/touchscreen/focaltech_touch/focaltech_common.h
+++ drivers/input/touchscreen/focaltech_touch/focaltech_common.h
@@ -2,7 +2,8 @@
  *
  * FocalTech fts TouchScreen driver.
  *
- * Copyright (c) 2012-2019, Focaltech Ltd. All rights reserved.
+ * Copyright (c) 2012-2018, Focaltech Ltd. All rights reserved.
+ * Copyright (C) 2020 XiaoMi, Inc.
  *
  * This software is licensed under the terms of the GNU General Public
  * License version 2, as published by the Free Software Foundation, and
@@ -36,14 +37,14 @@
 /*****************************************************************************
 * Macro definitions using #define
 *****************************************************************************/
-#define FTS_DRIVER_VERSION      "Focaltech V3.1 20190807"
+#define FTS_DRIVER_VERSION                  "Focaltech V2.2 20180321"
 
 #define BYTE_OFF_0(x)           (u8)((x) & 0xFF)
-#define BYTE_OFF_8(x)           (u8)(((x) >> 8) & 0xFF)
-#define BYTE_OFF_16(x)          (u8)(((x) >> 16) & 0xFF)
-#define BYTE_OFF_24(x)          (u8)(((x) >> 24) & 0xFF)
+#define BYTE_OFF_8(x)           (u8)((x >> 8) & 0xFF)
+#define BYTE_OFF_16(x)          (u8)((x >> 16) & 0xFF)
+#define BYTE_OFF_24(x)          (u8)((x >> 24) & 0xFF)
 #define FLAGBIT(x)              (0x00000001 << (x))
-#define FLAGBITS(x, y)          ((0xFFFFFFFF >> (32 - (y) - 1)) & (0xFFFFFFFF << (x)))
+#define FLAGBITS(x, y)          ((0xFFFFFFFF >> (32 - (y) - 1)) << (x))
 
 #define FLAG_ICSERIALS_LEN      8
 #define FLAG_HID_BIT            10
@@ -54,20 +55,9 @@
 #define FTS_CHIP_IDC            ((FTS_CHIP_TYPE & FLAGBIT(FLAG_IDC_BIT)) == FLAGBIT(FLAG_IDC_BIT))
 #define FTS_HID_SUPPORTTED      ((FTS_CHIP_TYPE & FLAGBIT(FLAG_HID_BIT)) == FLAGBIT(FLAG_HID_BIT))
 
-#define FTS_CHIP_TYPE_MAPPING   { \
-	{0x01, 0x58, 0x22, 0x58, 0x22, 0x00, 0x00, 0x58, 0x2C}, \
-	{0x02, 0x54, 0x22, 0x54, 0x22, 0x00, 0x00, 0x54, 0x2C}, \
-	{0x03, 0x64, 0x26, 0x64, 0x26, 0x00, 0x00, 0x79, 0x1C}, \
-	{0x04, 0x33, 0x67, 0x64, 0x26, 0x00, 0x00, 0x79, 0x1C}, \
-	{0x05, 0x87, 0x16, 0x87, 0x16, 0x87, 0xA6, 0x00, 0x00}, \
-	{0x06, 0x87, 0x36, 0x87, 0x36, 0x87, 0xC6, 0x00, 0x00}, \
-	{0x07, 0x80, 0x06, 0x80, 0x06, 0x80, 0xC6, 0x80, 0xB6}, \
-	{0x08, 0x86, 0x06, 0x86, 0x06, 0x86, 0xA6, 0x00, 0x00}, \
-	{0x09, 0x86, 0x07, 0x86, 0x07, 0x86, 0xA7, 0x00, 0x00}, \
-	{0x0A, 0xE7, 0x16, 0x87, 0x16, 0xE7, 0xA6, 0x87, 0xB6}, \
-	{0x0B, 0x14, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}, \
-}
+#define FTS_CHIP_TYPE_MAPPING {{0x82,0x54, 0x22, 0x54, 0x22, 0x54, 0x2D, 0x54, 0x2E}}
 
+#define I2C_BUFFER_LENGTH_MAXINUM           256
 #define FILE_NAME_LENGTH                    128
 #define ENABLE                              1
 #define DISABLE                             0
@@ -75,7 +65,7 @@
 #define INVALID                             0
 #define FTS_CMD_START1                      0x55
 #define FTS_CMD_START2                      0xAA
-#define FTS_CMD_START_DELAY                 12
+#define FTS_CMD_START_DELAY                 10
 #define FTS_CMD_READ_ID                     0x90
 #define FTS_CMD_READ_ID_LEN                 4
 #define FTS_CMD_READ_ID_LEN_INCELL          1
@@ -89,18 +79,16 @@
 #define FTS_REG_CHIP_ID                     0xA3
 #define FTS_REG_CHIP_ID2                    0x9F
 #define FTS_REG_POWER_MODE                  0xA5
-#define FTS_REG_POWER_MODE_SLEEP            0x03
+#define FTS_REG_POWER_MODE_SLEEP_VALUE      0x03
 #define FTS_REG_FW_VER                      0xA6
 #define FTS_REG_VENDOR_ID                   0xA8
 #define FTS_REG_LCD_BUSY_NUM                0xAB
 #define FTS_REG_FACE_DEC_MODE_EN            0xB0
-#define FTS_REG_FACTORY_MODE_DETACH_FLAG    0xB4
 #define FTS_REG_FACE_DEC_MODE_STATUS        0x01
 #define FTS_REG_IDE_PARA_VER_ID             0xB5
 #define FTS_REG_IDE_PARA_STATUS             0xB6
 #define FTS_REG_GLOVE_MODE_EN               0xC0
 #define FTS_REG_COVER_MODE_EN               0xC1
-#define FTS_REG_REPORT_RATE                 0x88
 #define FTS_REG_CHARGER_MODE_EN             0x8B
 #define FTS_REG_GESTURE_EN                  0xD0
 #define FTS_REG_GESTURE_OUTPUT_ADDRESS      0xD3
@@ -112,15 +100,14 @@
 #define FTS_SYSFS_ECHO_OFF(buf)     (buf[0] == '0')
 
 #define kfree_safe(pbuf) do {\
-	if (pbuf) {\
-		kfree(pbuf);\
-		pbuf = NULL;\
-	}\
+    if (pbuf) {\
+        kfree(pbuf);\
+        pbuf = NULL;\
+    }\
 } while(0)
 
 /*****************************************************************************
-*  Alternative mode (When something goes wrong,
-* the modules may be able to solve the problem.)
+*  Alternative mode (When something goes wrong, the modules may be able to solve the problem.)
 *****************************************************************************/
 /*
  * point report check
@@ -128,53 +115,48 @@
  */
 #define FTS_POINT_REPORT_CHECK_EN               0
 
+
 /*****************************************************************************
 * Global variable or extern global variabls/functions
 *****************************************************************************/
 struct ft_chip_t {
-	u64 type;
-	u8 chip_idh;
-	u8 chip_idl;
-	u8 rom_idh;
-	u8 rom_idl;
-	u8 pb_idh;
-	u8 pb_idl;
-	u8 bl_idh;
-	u8 bl_idl;
+    u64 type;
+    u8 chip_idh;
+    u8 chip_idl;
+    u8 rom_idh;
+    u8 rom_idl;
+    u8 pb_idh;
+    u8 pb_idl;
+    u8 bl_idh;
+    u8 bl_idl;
 };
+int init_tp_selftest(struct i2c_client * client);
 
 struct ts_ic_info {
-	bool is_incell;
-	bool hid_supported;
-	struct ft_chip_t ids;
+    bool is_incell;
+    bool hid_supported;
+    struct ft_chip_t ids;
 };
 
 /*****************************************************************************
 * DEBUG function define here
 *****************************************************************************/
 #if FTS_DEBUG_EN
-#define FTS_DEBUG(fmt, args...) do { \
-	printk("[FTS_TS]%s:"fmt"\n", __func__, ##args); \
-} while (0)
-
-#define FTS_FUNC_ENTER() do { \
-	printk("[FTS_TS]%s: Enter\n", __func__); \
-} while (0)
-
-#define FTS_FUNC_EXIT() do { \
-	printk("[FTS_TS]%s: Exit(%d)\n", __func__, __LINE__); \
-} while (0)
+#define FTS_DEBUG_LEVEL     1
+#if (FTS_DEBUG_LEVEL == 2)
+#define FTS_DEBUG(fmt, args...) printk("[FTS][%s]"fmt"\n", __func__, ##args)
+#else
+#define FTS_DEBUG(fmt, args...) printk("[FTS]"fmt"\n", ##args)
+#endif
+#define FTS_FUNC_ENTER() printk("[FTS]%s: Enter\n", __func__)
+#define FTS_FUNC_EXIT()  printk("[FTS]%s: Exit(%d)\n", __func__, __LINE__)
 #else /* #if FTS_DEBUG_EN*/
 #define FTS_DEBUG(fmt, args...)
 #define FTS_FUNC_ENTER()
 #define FTS_FUNC_EXIT()
 #endif
 
-#define FTS_INFO(fmt, args...) do { \
-	printk(KERN_INFO "[FTS_TS/I]%s:"fmt"\n", __func__, ##args); \
-} while (0)
+#define FTS_INFO(fmt, args...) printk(KERN_INFO "[FTS][Info]"fmt"\n", ##args)
+#define FTS_ERROR(fmt, args...) printk(KERN_ERR "[FTS][Error]"fmt"\n", ##args)
 
-#define FTS_ERROR(fmt, args...) do { \
-	printk(KERN_ERR "[FTS_TS/E]%s:"fmt"\n", __func__, ##args); \
-} while (0)
 #endif /* __LINUX_FOCALTECH_COMMON_H__ */
